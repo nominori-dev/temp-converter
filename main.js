9c84b680df63f75cedc2b256244de87b335b75e3
nominori-dev <antonshimchico37@gmail.com>
2021-07-09 22:28:30 +0200

    style update

--- main.js
+++ main.js
@@ -2,20 +2,26 @@ const path = require('path');
 const url = require('url');
 const {app, BrowserWindow} = require('electron');
 const { create } = require('domain');
+const {session}   = require('electron');
 
 let win;
 
 function createWindow(){
     win = new BrowserWindow({width:700,height:500,
-         icon: __dirname + "/img/icon.png"});
+         icon: __dirname + "/img/icon.png", webPreferences:{
+             devTools: false
+         }});
 
+    win.setResizable(false);
+    win.removeMenu();
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
 
-    win.webContents.openDevTools();
+    session.defaultSession.clearStorageData();
+
 
     win.on('closed', ()=>{
         win = null;
